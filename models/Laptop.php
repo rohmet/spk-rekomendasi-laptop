98a4a4de2b79b238df725aa33db66c145a354152
rohmet <harahmanabdarib@gmail.com>
2025-11-29 02:53:37 +0800

    feat: tambah fitur untuk menghapus dan menambah data laptop di dashboard admin

--- models/Laptop.php
+++ models/Laptop.php
@@ -22,6 +22,46 @@ class Laptop {
         $result = $this->conn->query($query);
         return $result;
     }
+
+    // FUNGSI: Tambah Data (Create)
+    public function insertLaptop($data) {
+        $query = "INSERT INTO " . $this->table_name . " 
+                  (brand, model_name, price, ram_gb, weight_kg, processor, gpu, screen_resolution, memory_type, os)
+                  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        
+        $stmt = $this->conn->prepare($query);
+        
+        // Binding parameter (s = string, d = double/float, i = integer)
+        $stmt->bind_param("ssdidsssss", 
+            $data['brand'], 
+            $data['model_name'], 
+            $data['price'], 
+            $data['ram_gb'], 
+            $data['weight_kg'], 
+            $data['processor'], 
+            $data['gpu'], 
+            $data['screen_resolution'], 
+            $data['memory_type'], 
+            $data['os']
+        );
+        
+        if($stmt->execute()){
+            return true;
+        }
+        return false;
+    }
+
+    // FUNGSI: Hapus Data (Delete)
+    public function deleteLaptop($id) {
+        $query = "DELETE FROM " . $this->table_name . " WHERE id_laptop = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("i", $id);
+
+        if($stmt->execute()){
+            return true;
+        }
+        return false;
+    }
 }
 
 ?>
